e38089baa5e889736d31e67f4703cc36ed3b2c94
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2026-06-01 18:11:22 +0900

    feat: add user, post, and comment models

---
+++ db/migrate/20260601090755_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :posts do |t|
+      t.text :title
+      t.text :link
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
