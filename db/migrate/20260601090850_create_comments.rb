72c123813eb75d6bb6b03d67338a08f8eeaeda5e
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2026-06-01 18:39:00 +0900

    feat: add foreign keys and score columns to posts and comments

--- db/migrate/20260601090850_create_comments.rb
+++ db/migrate/20260601090850_create_comments.rb
@@ -1,7 +1,10 @@
 class CreateComments < ActiveRecord::Migration[8.1]
   def change
     create_table :comments do |t|
-      t.text :body
+      t.text :body, null: false
+      t.integer :score, null: false, default: 0
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
